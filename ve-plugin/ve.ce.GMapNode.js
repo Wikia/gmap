5dc5b07bb5595aeaac4a4796d2e15872e65eb7a4
Inez Korczyński <inez@wikia-inc.com>
2013-07-26 15:05:13 -0700

    Refactored ve.ce.GMapNode and added comments

--- ve-plugin/ve.ce.GMapNode.js
+++ ve-plugin/ve.ce.GMapNode.js
@@ -1,13 +1,27 @@
+/**
+ * ContentEditable GMap node.
+ *
+ * @class
+ * @extends ve.ce.LeafNode
+ *
+ * @constructor
+ * @param {ve.dm.GMapNode} model Model to observe
+ * @param {Object} [config] Config options
+ */
 ve.ce.GMapNode = function VeCeGMapNode( model, config ) {
+	// Parent constructor
 	ve.ce.LeafNode.call( this, model, config );
 
+	// Mixin constructors
 	ve.ce.ProtectedNode.call( this );
-
 	ve.ce.FocusableNode.call( this );
 
+	// Setup
 	this.$image = $( '<img>' ).appendTo( this.$ );
-	this.updateMapUrl();
+	this.setImageURL();
 };
+
+/* Inheritance */
 
 ve.inheritClass( ve.ce.GMapNode, ve.ce.LeafNode );
 
@@ -15,32 +29,38 @@ ve.mixinClass( ve.ce.GMapNode, ve.ce.ProtectedNode );
 
 ve.mixinClass( ve.ce.GMapNode, ve.ce.FocusableNode );
 
+/* Static Properties */
+
 ve.ce.GMapNode.static.name = 'gmap';
 
 ve.ce.GMapNode.static.tagName = 'div';
 
+/* Methods */
+
+/**
+ * Handle attribute change events.
+ *
+ * @method
+ * @param {string} key Attribute key
+ * @param {string} from Old value
+ * @param {string} to New value
+ */
 ve.ce.GMapNode.prototype.onAttributeChange = function () {
-	this.updateMapUrl();
+	this.setImageURL();
 };
 
-ve.ce.GMapNode.prototype.updateMapUrl = function() {
+ve.ce.GMapNode.prototype.setImageURL = function() {
 	var params = this.model.getAttribute( 'params' );
 	this.$image
 		.attr(
 			'src',
-			this.getStaticMapUrl(
-				params.lat,
-				params.long,
-				params.width,
-				params.height,
-				params.zoom
-			)
+			this.generateImageURL( params.lat, params.long, params.width, params.height, params.zoom )
 		)
 		.height( params.height )
 		.width( params.width );
 };
 
-ve.ce.GMapNode.prototype.getStaticMapUrl = function( latitude, longitude, width, height, zoom ) {
+ve.ce.GMapNode.prototype.generateImageURL = function( latitude, longitude, width, height, zoom ) {
 	var params = [];
 	params.push( 'markers=' + latitude + ',' + longitude );
 	params.push( 'size=' + width + 'x' + height );
@@ -49,5 +69,7 @@ ve.ce.GMapNode.prototype.getStaticMapUrl = function( latitude, longitude, width,
 	params.push( 'sensor=false' );
 	return 'http://maps.googleapis.com/maps/api/staticmap?' + params.join( '&' );
 };
+
+/* Registration */
 
 ve.ce.nodeFactory.register( ve.ce.GMapNode );
